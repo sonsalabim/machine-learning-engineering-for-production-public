8c3b55b73ca7a8fa9ee3126b5e4301e1f11800ea
Sonja <sonja@Sonjas-MacBook-Pro.local>
2023-11-03 17:35:29 -0400

    Harmonizing sklearn version

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -11,6 +11,7 @@ app = FastAPI(title="Predicting Wine Class with batching")
 # Open classifier in global scope
 with open("models/wine-95-fixed.pkl", "rb") as file:
     clf = pickle.load(file)
+    clf.__getstate__()['_sklearn_version']
 
 class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_items=13, max_items=13)]
