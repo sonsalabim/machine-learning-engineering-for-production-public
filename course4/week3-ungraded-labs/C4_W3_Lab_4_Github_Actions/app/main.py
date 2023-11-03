a35716a6f5bb8482db7f5f68d8bf89f50631a6fa
Sonja <sonja@Sonjas-MacBook-Pro.local>
2023-11-03 17:16:03 -0400

    Adding comment to main

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,16 +4,16 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
+#comment
+
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
 with open("models/wine-95-fixed.pkl", "rb") as file:
     clf = pickle.load(file)
 
-
 class Wine(BaseModel):
     batches: List[conlist(item_type=float, min_items=13, max_items=13)]
-
 
 @app.post("/predict")
 def predict(wine: Wine):
